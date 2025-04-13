3ecdbdb65ebb8e431d5424d7b6be74f3d3b7471b
Forexfusion <shubhamtagalpallewar59@gmail.com>
2025-04-13 22:48:51 +0530

    Update page.tsx

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -13,7 +13,7 @@ export default function Pricing() {
         <div className="border border-white p-6 rounded w-64 shadow bg-[#0e0e0e]">
           <h3 className="font-bold text-xl mb-2">Core</h3>
           <p className="mb-2 text-gray-300">“Smart Start for Every Trader”</p>
-          <span className="block font-semibold mb-4">₹1,000/month</span>
+          <span className="block font-semibold mb-4">₹19999/month</span>
           <Link href="/pay/crypto/core">
             <button className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition">
               Get Core
@@ -25,7 +25,7 @@ export default function Pricing() {
         <div className="border border-white p-6 rounded w-64 shadow bg-[#0e0e0e]">
           <h3 className="font-bold text-xl mb-2">Pro</h3>
           <p className="mb-2 text-gray-300">“Power & Performance Combined”</p>
-          <span className="block font-semibold mb-4">₹5,000/month</span>
+          <span className="block font-semibold mb-4">₹24999/month</span>
           <Link href="/pay/crypto/pro">
             <button className="bg-yellow-500 text-white px-4 py-2 rounded hover:bg-yellow-600 transition">
               Get Pro
@@ -37,7 +37,7 @@ export default function Pricing() {
         <div className="border border-white p-6 rounded w-64 shadow bg-[#0e0e0e]">
           <h3 className="font-bold text-xl mb-2">Apex</h3>
           <p className="mb-2 text-gray-300">“Unleash Elite Trading Mastery”</p>
-          <span className="block font-semibold mb-4">₹10,000/month</span>
+          <span className="block font-semibold mb-4">₹49999/month</span>
           <Link href="/pay/crypto/apex">
             <button className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700 transition">
               Get Apex
