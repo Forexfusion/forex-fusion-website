c66186217f27e1049257ebed8148dd6df26cc0de
Forexfusion <shubhamtagalpallewar59@gmail.com>
2025-04-13 21:34:54 +0530

    Update PricingCard.tsx

--- src/components/PricingCard.tsx
+++ src/components/PricingCard.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useRouter } from "next/navigation";
+import { useRouter } from 'next/navigation';
 
 const PricingCard = ({ plan }: { plan: string }) => {
   const router = useRouter();
@@ -7,13 +7,13 @@ const PricingCard = ({ plan }: { plan: string }) => {
   return (
     <button
       onClick={() => router.push(`/pay/select/${plan.toLowerCase()}`)}
-      className={`py-2 px-4 rounded-full text-white hover:scale-105 transition
+      className="py-2 px-4 rounded-full text-white hover:scale-105 transition
         bg-gradient-to-r
         ${
           plan === 'Core' ? 'from-green-400 to-blue-500' :
           plan === 'Pro' ? 'from-purple-500 to-pink-500' :
           'from-yellow-400 to-orange-500'
-        }`}
+        }"
     >
       {`Get ${plan}`}
     </button>
