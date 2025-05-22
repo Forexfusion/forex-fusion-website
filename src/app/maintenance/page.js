d02d60b4a0301ce0a40853191eaf550c950b2ffe
Forexfusion <shubhamtagalpallewar59@gmail.com>
2025-05-22 21:05:11 +0530

    Create page.js

---
+++ src/app/maintenance/page.js
@@ -0,0 +1,8 @@
+export default function Maintenance() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Our website is under maintenance.</h1>
+      <p>We'll be back shortly. Thanks for your patience!</p>
+    </div>
+  );
+}
